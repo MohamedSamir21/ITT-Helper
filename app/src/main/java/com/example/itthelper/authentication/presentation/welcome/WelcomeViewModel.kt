29ba8c9c56e618206a2642423e2444d2a9bf26eb
mo <mo@gmail.com>
2024-03-05 17:46:15 +0200

    feat: Implement WelcomeViewModel.kt

--- app/src/main/java/com/example/itthelper/authentication/presentation/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/example/itthelper/authentication/presentation/welcome/WelcomeViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.itthelper.authentication.presentation.welcome
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.itthelper.authentication.presentation.util.OnBoardingPage
 
-class WelcomeViewModel: ViewModel() {
+class WelcomeViewModel : ViewModel() {
+    private val _state = mutableStateOf(
+        WelcomeScreenState(
+            onBoardingPages = listOf(
+                OnBoardingPage.First,
+                OnBoardingPage.Second,
+                OnBoardingPage.Third
+            )
+        )
+    )
+    val state: State<WelcomeScreenState>
+        get() = _state
 
+    fun updateCurrentPageIndex(index: Int){
+        _state.value = _state.value.copy(currentPageIndex = index)
+    }
 }
