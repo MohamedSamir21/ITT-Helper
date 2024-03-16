3b7631344f853dcaa4975773b761368448367e11
mo <mo@gmail.com>
2024-03-16 17:41:20 +0200

    feat: Add DI modules

---
+++ app/src/main/java/com/example/itthelper/authentication/di/AuthModule.kt
@@ -0,0 +1,44 @@
+package com.example.itthelper.authentication.di
+
+import android.content.Context
+import com.example.itthelper.authentication.data.repository.DataStoreRepositoryImpl
+import com.example.itthelper.authentication.data.local.DataStoreRepository
+import com.example.itthelper.authentication.data.remote.AuthenticationApi
+import com.example.itthelper.authentication.data.repository.AuthRepositoryImpl
+import com.example.itthelper.authentication.domain.repository.AuthRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthenticationApi(retrofit: Retrofit): AuthenticationApi {
+        return retrofit.create(AuthenticationApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataStoreRepository(
+        @ApplicationContext context: Context
+    ): DataStoreRepository {
+        return DataStoreRepositoryImpl(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthenticationRepository(
+        api: AuthenticationApi,
+        dataStoreRepository: DataStoreRepository
+    ): AuthRepository {
+        return AuthRepositoryImpl(api, dataStoreRepository)
+    }
+
+}
---
+++ app/src/main/java/com/example/itthelper/core/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.example.itthelper.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://192.168.1.5:8080/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+}
