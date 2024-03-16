e567c57563e5e13992c3a4a251ddc76821d32463
mo <mo@gmail.com>
2024-03-16 21:22:12 +0200

    chore: Add the screens to the NavGraphBuilder

--- app/src/main/java/com/example/itthelper/authentication/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/itthelper/authentication/presentation/navigation/NavGraph.kt
@@ -1,10 +1,17 @@
 package com.example.itthelper.authentication.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.itthelper.authentication.presentation.auth.AuthScreen
+import com.example.itthelper.authentication.presentation.login.LoginScreen
+import com.example.itthelper.authentication.presentation.login.LoginViewModel
+import com.example.itthelper.authentication.presentation.register.RegisterScreen
+import com.example.itthelper.authentication.presentation.register.RegisterViewModel
 import com.example.itthelper.authentication.presentation.welcome.WelcomeScreen
+import com.example.itthelper.authentication.presentation.welcome.WelcomeViewModel
 
 @Composable
 fun SetupNavGraph(
@@ -18,20 +25,34 @@ fun SetupNavGraph(
         composable(
             route = Screen.WELCOME.route
         ) {
-            WelcomeScreen()
+            val viewModel: WelcomeViewModel = hiltViewModel()
+            WelcomeScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
         }
-        composable(route = Screen.AUTH.route){
-
+        composable(route = Screen.AUTH.route) {
+            AuthScreen(
+                navController = navController
+            )
         }
         composable(
             route = Screen.REGISTER.route
-        ){
-
+        ) {
+            val registerViewModel: RegisterViewModel = hiltViewModel()
+            RegisterScreen(
+                navController = navController,
+                viewModel = registerViewModel
+            )
         }
         composable(
             route = Screen.LOGIN.route
-        ){
-
+        ) {
+            val loginViewModel: LoginViewModel = hiltViewModel()
+            LoginScreen(
+                navController = navController,
+                viewModel = loginViewModel
+            )
         }
     }
 }
