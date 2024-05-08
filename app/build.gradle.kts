16ffd6cedaa0df52472c69b60bbaa226e20c44c7
mo <mo@gmail.com>
2024-05-08 17:01:36 +0300

    fix: Add coreLibraryDesugaring to terminate the issue of Call requires API level 26 (current min is 24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
         }
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -68,6 +69,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
