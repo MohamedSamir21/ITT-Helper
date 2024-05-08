40423176e46fba309076ba6c546c4e81be395952
mo <mo@gmail.com>
2024-05-08 16:28:51 +0300

    remove: Get rid of Employment Market affairs since this tab is not needed as a requirement

--- app/src/main/java/com/example/itthelper/career_guidance_hub/presentation/util/TabContent.kt
+++ app/src/main/java/com/example/itthelper/career_guidance_hub/presentation/util/TabContent.kt
@@ -2,7 +2,6 @@ package com.example.itthelper.career_guidance_hub.presentation.util
 
 sealed class TabContent {
     object CareerPath : TabContent()
-    object EmploymentMarket : TabContent()
     object EventsWorkshops : TabContent()
     object CvsTips : TabContent()
     object GuideForInterviews : TabContent()
