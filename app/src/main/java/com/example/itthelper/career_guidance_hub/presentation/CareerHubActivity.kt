6a81eb2393958af30f5ef1ac72209dd45dd8524d
mo <mo@gmail.com>
2024-05-16 14:03:28 +0300

    refactor: Rename CareerHubFeature to CareerHubScaffold and move it to components package for more separation.

--- app/src/main/java/com/example/itthelper/career_guidance_hub/presentation/CareerHubActivity.kt
+++ app/src/main/java/com/example/itthelper/career_guidance_hub/presentation/CareerHubActivity.kt
@@ -3,30 +3,9 @@ package com.example.itthelper.career_guidance_hub.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.itthelper.R
-import com.example.itthelper.career_guidance_hub.presentation.components.BottomNavigationBar
-import com.example.itthelper.career_guidance_hub.presentation.components.TopAppBar
-import com.example.itthelper.career_guidance_hub.presentation.navigation.BottomNavigationItem
-import com.example.itthelper.career_guidance_hub.presentation.navigation.Screen
-import com.example.itthelper.career_guidance_hub.presentation.navigation.SetupNavGraph
+import com.example.itthelper.career_guidance_hub.presentation.components.CareerHubScaffold
 import com.example.itthelper.career_guidance_hub.presentation.navigation.bottomNavigationItems
 import com.example.itthelper.core.ui.theme.ITTHelperTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,100 +16,18 @@ class CareerHubActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ITTHelperTheme {
+                val viewModel: CareerHubViewModel = hiltViewModel()
                 val navController = rememberNavController()
-                var selectedNavigationItemIndex by rememberSaveable {
-                    mutableIntStateOf(0)
-                }
 
-                CareerHubFeature(
+                CareerHubScaffold(
+                    viewModel = viewModel,
                     navController = navController,
                     bottomNavigationItems = bottomNavigationItems
                 ) {
-                    selectedNavigationItemIndex = it
+                    viewModel.onSelectedNavigationItemIndex(it)
                 }
             }
         }
     }
-
 }
 
-@Composable
-private fun CareerHubFeature(
-    navController: NavHostController,
-    bottomNavigationItems: List<BottomNavigationItem>,
-    onSelectedNavigationItemIndex: (Int) -> Unit
-) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    var showTopAppBar by rememberSaveable {
-        mutableStateOf(true)
-    }
-    var showBottomAppBar by rememberSaveable {
-        mutableStateOf(true)
-    }
-    var showFAB by rememberSaveable {
-        mutableStateOf(true)
-    }
-    when (navBackStackEntry?.destination?.route) {
-        Screen.Profile.route -> {
-            showTopAppBar = false
-            showBottomAppBar = false
-            showFAB = false
-        }
-
-        Screen.ContactUs.route -> {
-            showTopAppBar = false
-            showBottomAppBar = false
-            showFAB = false
-        }
-
-        Screen.Feedback.route -> {
-            showTopAppBar = false
-            showBottomAppBar = false
-            showFAB = false
-        }
-
-        else -> {
-            showTopAppBar = true
-            showBottomAppBar = true
-            showFAB = true
-        }
-    }
-
-    Scaffold(
-        topBar = {
-            if (showTopAppBar)
-                TopAppBar(navController = navController)
-        },
-        bottomBar = {
-            if (showBottomAppBar)
-                BottomNavigationBar(
-                    bottomNavigationItems = bottomNavigationItems,
-                    navController = navController,
-                ) { newIndex ->
-                    onSelectedNavigationItemIndex(newIndex)
-                }
-        },
-        floatingActionButton = {
-            if (showFAB)
-                FloatingActionButton(
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.bot),
-                        contentDescription = stringResource(R.string.chat_bot)
-                    )
-                }
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            SetupNavGraph(
-                navController = navController,
-                startDestination = Screen.Home.route
-            )
-        }
-    }
-}
---
+++ app/src/main/java/com/example/itthelper/career_guidance_hub/presentation/components/CareerHubScaffold.kt
@@ -0,0 +1,75 @@
+package com.example.itthelper.career_guidance_hub.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.itthelper.R
+import com.example.itthelper.career_guidance_hub.presentation.CareerHubViewModel
+import com.example.itthelper.career_guidance_hub.presentation.navigation.BottomNavigationItem
+import com.example.itthelper.career_guidance_hub.presentation.navigation.Screen
+import com.example.itthelper.career_guidance_hub.presentation.navigation.SetupNavGraph
+
+@Composable
+fun CareerHubScaffold(
+    viewModel: CareerHubViewModel,
+    navController: NavHostController,
+    bottomNavigationItems: List<BottomNavigationItem>,
+    onSelectedNavigationItemIndex: (Int) -> Unit
+) {
+    val context = LocalContext.current
+    viewModel.updateCurrentRoute(navController.currentBackStackEntryAsState().value?.destination?.route)
+
+    Scaffold(
+        topBar = {
+            if (viewModel.showTopAppBarValue)
+                TopAppBar(navController = navController)
+        },
+        bottomBar = {
+            if (viewModel.showBottomAppBarValue)
+                BottomNavigationBar(
+                    bottomNavigationItems = bottomNavigationItems,
+                    navController = navController,
+                ) { newIndex ->
+                    onSelectedNavigationItemIndex(newIndex)
+                }
+        },
+        floatingActionButton = {
+            if (viewModel.showFABValue)
+                FloatingActionButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.bot),
+                        contentDescription = stringResource(R.string.chat_bot)
+                    )
+                }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            SetupNavGraph(
+                navController = navController,
+                startDestination = Screen.Home.route,
+                onUnauthorized = {
+                    viewModel.showUnauthorizedDialog()
+                    viewModel.setUnauthorizedMessage(it.asString(context))
+                }
+            )
+            if (viewModel.showUnauthorizedDialogValue)
+                UnauthorizedDialog(context, viewModel.unauthorizedMessageValue)
+        }
+    }
+}
